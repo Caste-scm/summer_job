138fd7435a40849cc24fc611e0e63cc8a126faec
Caste-scm <filippocastellan27@gmail.com>
2025-09-10 18:21:14 +0200

    Delete Circle.java

--- Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle extends Shape{
-    private double n;
-    public Circle(double n) {
-        this.n = n;
-    }
-    public double calculateArea(){
-        return 3.1416*n*n;
-    }
-}
